8066e322373363b30c91c963876e465afc0d90d7
felixmiranda1 <felix.miranda1@outlook.com>
2025-04-02 10:18:59 -0300

    Adding allowed hosts

--- heartbeat/settings.py
+++ heartbeat/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '.railway.app', '127.0.0.1', 'heartbeat-production.up.railway.app']
 
 
 
